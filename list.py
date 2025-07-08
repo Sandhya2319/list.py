ef7aad0973f1e81e6e95176ad91f0ba466daef2c
Sandhya2319 <140784762+Sandhya2319@users.noreply.github.com>
2025-07-09 01:26:38 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,42 @@
+s=[10,34,"python",2.34,True,3+4j]
+#APPEND
+s.append(54)
+print(s)
+#INSERT(index,value)
+print(s.insert(4,"html")
+#REMOVE
+print(s.remove(34))
+#POP(INDEX)
+print(s.pop(5))
+#POP
+print(s.pop())
+#CLEAR
+print(s.clear())
+-----------------------------------------------------------------------------------
+s=[10,34,"python",2.34,True,3+4j]
+#INDEX(value)
+print(s.index("python"))
+#ENUMERATOR
+for x in enumerate(s):
+    print(X)
+#COUNT
+print(s.count(10))
+#Copying the content of one list to another list
+s1=s.copy()
+print(s1)
+#REVERSE-- this elements in listobject itself
+s1=s.reverse()
+print(s1)
+print(s)
+#SORTING
+s.sort(reverse=False)
+print(s)
+#EXTEND--comining the lists
+p=[11,3,56]
+s.extend(p)
+print(s)
+
+
+
+
+
